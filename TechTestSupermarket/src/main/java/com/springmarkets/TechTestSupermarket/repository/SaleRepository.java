98d16ba017ba11058703c7425e6a113ebb161c43
franarayah <franarayah@gmail.com>
2025-11-16 20:46:25 -0300

    Fixing Impor on ProductRepository
    Correct the import of the JPA library

--- TechTestSupermarket/src/main/java/com/springmarkets/TechTestSupermarket/repository/SaleRepository.java
+++ TechTestSupermarket/src/main/java/com/springmarkets/TechTestSupermarket/repository/SaleRepository.java
@@ -4,5 +4,5 @@ import com.springmarkets.TechTestSupermarket.model.Sale;
 import org.springframework.data.jpa.repository.JpaRepository;
 
 public interface SaleRepository extends JpaRepository<Sale, Long> {
-  
+
 }
