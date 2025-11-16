f04cb6e5ff9f20aafb5cd0de34dafd2688789518
franarayah <franarayah@gmail.com>
2025-11-16 20:29:16 -0300

    Adding Repository Interfaces
    Fix relationship between Sale and DetailSail to avoid create a DetailSaleRepository.
    Adding Interfaces to manage data with JPA

--- TechTestSupermarket/src/main/java/com/springmarkets/TechTestSupermarket/model/Sale.java
+++ TechTestSupermarket/src/main/java/com/springmarkets/TechTestSupermarket/model/Sale.java
@@ -1,15 +1,14 @@
 package com.springmarkets.TechTestSupermarket.model;
 
-import jakarta.persistence.GeneratedValue;
-import jakarta.persistence.GenerationType;
-import jakarta.persistence.Id;
-import jakarta.persistence.ManyToOne;
+import jakarta.persistence.*;
 import lombok.AllArgsConstructor;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
 import lombok.Setter;
 
 import java.time.LocalDate;
+import java.util.ArrayList;
+import java.util.List;
 
 @Getter @Setter
 @AllArgsConstructor
@@ -25,4 +24,8 @@ public class Sale {
   //Relationship with the store
   @ManyToOne
   private Store store;
+
+  //Relationship with the sale details
+  @OneToMany(mappedBy = "sale")
+  private List<SaleDetail> saleDetails = new ArrayList<>();
 }
---
+++ TechTestSupermarket/src/main/java/com/springmarkets/TechTestSupermarket/repository/ProductRepository.java
@@ -0,0 +1,7 @@
+package com.springmarkets.TechTestSupermarket.repository;
+
+import com.springmarkets.TechTestSupermarket.model.Product;
+
+public interface ProductRepository extends JpaRepository<Product, Long> {
+
+}
