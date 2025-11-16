bb68af0f2fbe2e991e9d97c9faf66b25bd9b0fad
franarayah <franarayah@gmail.com>
2025-11-16 02:37:04 -0300

    Establishing the project foundation
    Added the basic Spring Boot project structure and the initial files
    generated by Spring Initializr.

---
+++ TechTestSupermarket/src/main/java/com/springmarkets/TechTestSupermarket/TechTestSupermarketApplication.java
@@ -0,0 +1,13 @@
+package com.springmarkets.TechTestSupermarket;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TechTestSupermarketApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TechTestSupermarketApplication.class, args);
+	}
+
+}
---
+++ TechTestSupermarket/src/test/java/com/springmarkets/TechTestSupermarket/TechTestSupermarketApplicationTests.java
@@ -0,0 +1,13 @@
+package com.springmarkets.TechTestSupermarket;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TechTestSupermarketApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
